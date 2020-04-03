aa7ff1d5fdd241b5bea579a63d17fe8a81aea514
Samuel Craymer <s.craymer@idscorporation.com>
2020-04-03 13:22:29 -0400

    fix(unit-test): resolve unit test issues

--- src/app/shared/layout/scroll-hide.directive.spec.ts
+++ src/app/shared/layout/scroll-hide.directive.spec.ts
@@ -1,8 +1,28 @@
+import { ElementRef, Renderer2 } from '@angular/core';
+import { async, TestBed } from '@angular/core/testing';
+
 import { ScrollHideDirective } from './scroll-hide.directive';
 
 describe('ScrollHideDirective', () => {
+
+  let el: ElementRef;
+  let renderer: Renderer2;
+
+  beforeEach(async( () => {
+      TestBed.configureTestingModule({
+          providers: [MockElementRef, Renderer2]
+      }).compileComponents();
+
+      el = TestBed.get(MockElementRef);
+      renderer = TestBed.get(Renderer2);
+  }));
+
   it('should create an instance', () => {
-    const directive = new ScrollHideDirective();
+    const directive = new ScrollHideDirective(el, renderer);
     expect(directive).toBeTruthy();
   });
 });
+
+export class MockElementRef extends ElementRef<Element> {
+  constructor() { super(null); }
+}
