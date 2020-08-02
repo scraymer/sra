1c1971bb4a65fe86792a8e47c8ccdb2ae01e8bff
Samuel Craymer <s.craymer@idscorporation.com>
2020-08-01 20:00:37 -0400

    feature(auth): add browser user-agent prefix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,6 @@ export const environment = {
     redirectUrl: 'https://scraymer.github.io/sra/auth/login',
     scope: ['mysubreddits', 'read'],
     urlPrefix: 'https://reddit.com/',
-    userAgent: 'browser:ca.craymer.sam.sra:v0.1.0 (by /u/simple-reddit-app)'
+    userAgent: navigator.userAgent + ':ca.craymer.sam.sra:v0.1.0 (by /u/simple-reddit-app)'
   }
 };
