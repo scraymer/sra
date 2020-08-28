4dd2dc27d6a4886136e53cf99961c23aa9235e18
Samuel Craymer <s.craymer@idscorporation.com>
2020-08-28 11:58:12 -0400

    fix(lint): supress bitwise tslint warning

--- src/app/core/reddit/reddit.service.ts
+++ src/app/core/reddit/reddit.service.ts
@@ -158,7 +158,10 @@ export class RedditService {
 
         if (!this.storage.has(RedditConstant.AUTH_STATE_KEY)) {
             this.storage.set(RedditConstant.AUTH_STATE_KEY, '10000000-1000-4000-8000-100000000000'
-                .replace(/[018]/g, (c: any) => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)));
+                .replace(/[018]/g, (c: any) => (
+                    // tslint:disable-next-line:no-bitwise
+                    c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4
+                ).toString(16)));
         }
 
         return this.storage.get(RedditConstant.AUTH_STATE_KEY);
