e86214c6f6ae3d2c583dc1f2c79b58ba6e1964d7
Samuel Craymer <s.craymer@idscorporation.com>
2021-10-16 00:42:05 -0400

    fix(tests): remove all blank spec files

--- src/app/shared/layout/scrollbar/scrollbar.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ScrollbarDirective } from './scrollbar.directive';
-
-describe('ScrollbarDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ScrollbarDirective();
-    expect(directive).toBeTruthy();
-  });
-});
